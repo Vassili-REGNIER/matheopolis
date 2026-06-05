c4e3cb10334a050db94e7b5a8f1d466557824f7a
ilian-marchitto <ilian.marchitto@etu.univ-amu.fr>
2026-06-05 17:23:03 +0200

    refactor: rename StudentContentAccessComponent to StudentContentManagementComponent
    - Updated references in the MatheoPanelComponent and NavigationComponent to reflect the new component name.
    - Added the StudentContentManagementComponent to manage game access for students.
    - Adjusted documentation to align with the updated component structure.

--- frontend/src/components/MatheoPanel/MatheoPanelComponent.ts
+++ frontend/src/components/MatheoPanel/MatheoPanelComponent.ts
@@ -6,7 +6,7 @@ import { AdminPanelComponent } from "./Views/AdminPanel/AdminPanelComponent.js";
 import { ClassManagementComponent } from "./Views/ClassManagement/ClassManagementComponent.js";
 import { ProfileComponent } from "./Views/Profile/ProfileComponent.js";
 import { ProgressComponent } from "./Views/Progress/ProgressComponent.js";
-import { StudentContentAccessComponent } from "./Views/StudentContentAccess/StudentContentAccessComponent.js";
+import { StudentContentManagementComponent } from "./Views/StudentContentManagement/StudentContentManagementComponent.js";
 import { StudentClassComponent } from "./Views/StudentClass/StudentClassComponent.js";
 import { NavigationComponent, type PanelViewId } from "./Navigation/NavigationComponent.js";
 
@@ -98,8 +98,8 @@ export class MatheoPanelComponent extends BaseComponent {
       this.activeView = new StudentClassComponent(host, this.user);
     } else if (viewId === "classes") {
       this.activeView = new ClassManagementComponent(host, this.services);
-    } else if (viewId === "student-content-access") {
-      this.activeView = new StudentContentAccessComponent(host, this.services);
+    } else if (viewId === "student-content-management") {
+      this.activeView = new StudentContentManagementComponent(host, this.services);
     } else {
       this.activeView = new AdminPanelComponent(host, this.services);
     }
